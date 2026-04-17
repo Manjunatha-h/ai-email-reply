51a6ce250a3d64d7132c92ebde83f634b99a2215
Manjunath <manjunathh6789@gmail.com>
2026-04-17 14:49:43 +0530

    build ai email reply backend using REST-API and gemini language model to generate response

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGenController.java
@@ -0,0 +1,21 @@
+package com.email.writer.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailGenController {
+
+    private final EmailGenService emailGenService;
+
+    public EmailGenController(EmailGenService emailGenService){
+        this.emailGenService = emailGenService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateMail(@RequestBody EmailRequest emailRequest){
+        String response = emailGenService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
