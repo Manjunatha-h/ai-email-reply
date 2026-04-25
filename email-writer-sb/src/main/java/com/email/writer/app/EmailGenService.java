2b67147e47709ad14d2067f5ed0cd01a32914dec
Manjunath <manjunathh6789@gmail.com>
2026-04-25 16:32:44 +0530

    changes the backend service from manual ai-reponse handling api call to chatModel api from Spring-AI simple processing and response generation

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGenService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGenService.java
@@ -2,6 +2,10 @@ package com.email.writer.app;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,9 +16,13 @@ import java.util.Map;
 @Service
 public class EmailGenService {
 
+    private final ChatModel chatModel;
+
+
     private final WebClient webClient;
 
-    public EmailGenService(WebClient.Builder webClientBuilder) {
+    public EmailGenService(ChatModel chatModel, WebClient.Builder webClientBuilder) {
+        this.chatModel = chatModel;
         this.webClient = webClientBuilder.build();
     }
 
@@ -24,81 +32,111 @@ public class EmailGenService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
-    public String generateEmailReply(EmailRequest emailRequest){
-        //Build a prompt
-        String prompt = buildPrompt(emailRequest);
+    public String generateEmailReply(EmailRequest emailRequest) {
 
-        //Craft a request
-        Map<String,Object> requestBody = Map.of(
-                "contents", new Object[]{
-                        Map.of(
-                                "parts", new Object[]{
-                                        Map.of("text", prompt)
-                                }
-                        )
-                }
+        var template = """
+                Generate a professional email reply for the following email content. Please don't generate a subject line,
+                    Use a {tone} tone,
+                    Original Email : {original_email}
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Map<String, Object> params = Map.of(
+                "tone", emailRequest.getTone(),
+                "original_email", emailRequest.getEmailContent()
         );
 
-        //Do request and get response
-        String response = webClient.post()
-                .uri(geminiApiUrl+geminiApiKey)
-                .header("Content-type","application/json")
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        //making use of webClient with block is not differable from restClient
+        Prompt prompt1 = promptTemplate.create(params);
 
-        //-----------------------------------------------------------------------------------
-        //restTemplate -> legacy, spring has it in maintainance(so no updation), block calls
-        //webClient -> non-block(asynchronous) reactive(Mono mainatins reactive data return)
-        //restClient -> modern and better replace for restTemplate, block nd non-reactive
-        //------------------------------------------------------------------------------------
+        return chatModel.call(prompt1).getResult().getOutput().getText();
 
-
-        // -----------------------------------------------
-        //Request reaches controller
-        //Controller method is invoked
-        //Mono pipeline is created (no API call yet)
-        //Controller returns Mono immediately
-        //Controller execution ends (no further role)
-        //Spring receives the returned Mono
-        //Spring internally subscribes
-        //API call is triggered after subscription
-        //Response arrives
-        //Body is converted to required type (String)
-        //Spring sends response to client
-        //--------------------------------------------------
-
-        //extract and return response
-
-        return extractResponseContent(response);
-
-    }
-
-    private String extractResponseContent(String response) {
-        try{
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(response);
-            return root.path("candidates")
-                    .get(0)
-                    .path("content")
-                    .path("parts")
-                    .get(0)
-                    .path("text")
-                    .asText();
-        }catch (Exception e){
-            return "Error Processing Request: "+ e.getMessage();
-        }
-    }
-
-    private String buildPrompt(EmailRequest emailRequest) {
-        StringBuilder prompt = new StringBuilder();
-        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line");
-        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
-            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
-        }
-        prompt.append("\n Original Email : \n").append(emailRequest.getEmailContent());
-        return prompt.toString();
     }
 }
+
+
+
+
+
+
+
+
+
+
+//Build a prompt
+//        String prompt = buildPrompt(emailRequest);
+
+
+//        //Craft a request
+//        Map<String,Object> requestBody = Map.of(
+//                "contents", new Object[]{
+//                        Map.of(
+//                                "parts", new Object[]{
+//                                        Map.of("text", prompt)
+//                                }
+//                        )
+//                }
+//        );
+//
+//        //Do request and get response
+//        String response = webClient.post()
+//                .uri(geminiApiUrl+geminiApiKey)
+//                .header("Content-type","application/json")
+//                .bodyValue(requestBody)
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .block();
+//        //making use of webClient with block is not differable from restClient
+//
+//        //-----------------------------------------------------------------------------------
+//        //restTemplate -> legacy, spring has it in maintainance(so no updation), block calls
+//        //webClient -> non-block(asynchronous) reactive(Mono mainatins reactive data return)
+//        //restClient -> modern and better replace for restTemplate, block nd non-reactive
+//        //------------------------------------------------------------------------------------
+//
+//
+//        // -----------------------------------------------
+//        //Request reaches controller
+//        //Controller method is invoked
+//        //Mono pipeline is created (no API call yet)
+//        //Controller returns Mono immediately
+//        //Controller execution ends (no further role)
+//        //Spring receives the returned Mono
+//        //Spring internally subscribes
+//        //API call is triggered after subscription
+//        //Response arrives
+//        //Body is converted to required type (String)
+//        //Spring sends response to client
+//        //--------------------------------------------------
+//
+//        //extract and return response
+//
+//        return extractResponseContent(response);
+//
+//    }
+//
+//    private String extractResponseContent(String response) {
+//        try{
+//            ObjectMapper mapper = new ObjectMapper();
+//            JsonNode root = mapper.readTree(response);
+//            return root.path("candidates")
+//                    .get(0)
+//                    .path("content")
+//                    .path("parts")
+//                    .get(0)
+//                    .path("text")
+//                    .asText();
+//        }catch (Exception e){
+//            return "Error Processing Request: "+ e.getMessage();
+//        }
+//    }
+//
+//    private String buildPrompt(EmailRequest emailRequest) {
+//        StringBuilder prompt = new StringBuilder();
+//        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line");
+//        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+//            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+//        }
+//        prompt.append("\n Original Email : \n").append(emailRequest.getEmailContent());
+//        return prompt.toString();
+//    }
+//}
